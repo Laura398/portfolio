4181c5a35f6c7eab01161c75bb65669789d383ba
Laura <laura@upschool.fr>
2024-02-18 22:35:06 +0100

    started about page

--- src/views/pages/about/About.tsx
+++ src/views/pages/about/About.tsx
@@ -1,10 +1,16 @@
 import "./About.scss";
+import AboutBackground from "./Background";
 
 export default function About() {
     return (
-        <div id="about-container">
-            <div id="about"></div>
-            <h1>About</h1>
+        <div id="about">
+            <AboutBackground />
+            <div id="about-container" className="about-element">
+                <h1>About Me</h1>
+                <img src="/assets/avatar2.jpeg" alt="Avatar" />
+                <p>I'm Laura Mathieu, a web developer based in France. I'm currently a student at the school Ynov Informatique in Toulouse, studying web developement. I'm passionate about using technology to solve problems and improve the world around us. I'm currently seeking opportunities to work on projects that make a positive impact on the world.</p>
+            </div>
+            <div id="stack" className="about-element"></div>
         </div>
     )
 }
---
+++ src/views/pages/about/Background.tsx
@@ -0,0 +1,13 @@
+import './Background.scss';
+
+export default function AboutBackground () {
+    return (
+        <>
+        <div className='circle xxlarge shade1'></div>
+        <div className='circle xlarge shade2'></div>
+        <div className='circle large shade3'></div>
+        <div className='circle mediun shade4'></div>
+        <div className='circle small shade5'></div>
+    </>
+    )
+}
