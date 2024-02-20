c4e18acc2d88e9f274f4d1c05954e5b5010ff8f4
Laura <laura@upschool.fr>
2024-02-20 11:32:15 +0100

    phone header link

--- src/views/common/header/PhoneHeader.tsx
+++ src/views/common/header/PhoneHeader.tsx
@@ -30,7 +30,7 @@ export default function PhoneHeader() {
             <Button><a href="#" className='button' onClick={toggleAcordion}>Home</a></Button>
             <Button><a href="#about" className='button' onClick={toggleAcordion}>About</a></Button>
             <Button><a href="#stack" className='button' onClick={toggleAcordion}>Stack</a></Button>
-            <Button><a href="#portfolio" className='button' onClick={toggleAcordion}>Portfolio</a></Button>
+            <Button><a href="#home-portfolio" className='button' onClick={toggleAcordion}>Portfolio</a></Button>
             <Button><a href="#contact" className='button' onClick={toggleAcordion}>Contact</a></Button>
             <Button id="header-fb">
               <a target="_blank" href="https://www.linkedin.com/in/laura-mathieu-95385a18b"><LinkedInIcon className="linkedin-icon" fontSize='large' sx={{ color: "#61dafb" }} /></a>
