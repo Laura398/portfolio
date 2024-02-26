4fd7432929403ac4ea991469f7001de933039cf8
Laura <laura@upschool.fr>
2024-02-26 22:35:59 +0100

    fix build error

--- i18n.ts
+++ i18n.ts
@@ -2,23 +2,27 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-xhr-backend";
 
-i18n
-.use(Backend)
-.use(initReactI18next)
-.init({
-    lng: localStorage.getItem("lang") || "en",
-    fallbackLng: "en",
-    interpolation: {
-        escapeValue: false,
-    },
-    resources: {
-        en: {
+async function initI18n() {
+    await i18n
+        .use(Backend)
+        .use(initReactI18next)
+        .init({
+        lng: localStorage.getItem("lang") || "en",
+        fallbackLng: "en",
+        interpolation: {
+            escapeValue: false,
+        },
+        resources: {
+            en: {
             translation: await import("./public/locales/en/translation.json"),
-        },
-        fr: {
+            },
+            fr: {
             translation: await import("./public/locales/fr/translation.json"),
+            },
         },
-    },
-});
+        });
+}
+
+    initI18n();
 
 export default i18n;
