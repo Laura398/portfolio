2c7e66e0666747a631eb9a97f966c2f3b430fcfb
Laura <laura@upschool.fr>
2024-04-12 15:05:45 +0200

    add legal mentions in french

--- src/views/common/footer/Footer.tsx
+++ src/views/common/footer/Footer.tsx
@@ -1,12 +1,31 @@
 import { useTranslation } from 'react-i18next';
 import './Footer.scss';
+import React from 'react';
+import { DialogContent, DialogTitle, Modal, ModalClose, ModalDialog, Stack } from '@mui/joy';
+import LegalMentions from '../../pages/legal-mentions/LegalMentions';
 
 export default function Footer() {
     const { t } = useTranslation();
+    const [open, setOpen] = React.useState(false);
 
     return (
         <footer className="footer">
+            <p style={{cursor: "pointer"}} onClick={() => setOpen(true)}>Mentions Légales</p>
             © Laura Mathieu - 2024 - {t("allRightsReserved")}
+
+            <React.Fragment>
+                <Modal open={open} onClose={() => setOpen(false)}>
+                    <ModalDialog layout="fullscreen" sx={{backgroundColor: "black", color: "white", overflow: "auto"}}>
+                        <ModalClose color="primary" sx={{color: "white", "&:hover": {color: "black"}}} />
+                        <Stack direction="column" justifyContent="center" alignItems="center" gap={4}>
+                            <DialogTitle>Mentions Légales</DialogTitle>
+                            <DialogContent>
+                                <LegalMentions />
+                            </DialogContent>
+                        </Stack>
+                    </ModalDialog>
+                </Modal>
+            </React.Fragment>
         </footer>
     )
 }
---
+++ src/views/pages/legal-mentions/LegalMentions.tsx
@@ -0,0 +1,52 @@
+import { Box, Typography } from "@mui/joy";
+
+export default function LegalMentions() {
+    return (
+        <Box width={{sm: "95%", md: "70%"}} margin="auto">
+            <Typography textColor="white">
+                Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance en l'économie numérique, il est précisé aux utilisateurs du site Laura Mathieu - Portfolio l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi.
+            </Typography>
+            <Typography mt={4} textColor="white">
+                <strong>Édition du site</strong> :
+            </Typography> 
+            <Typography textColor="white">
+                Le présent site, accessible à l’URL https://www.portfolio.lauramathieu.com/, est édité par Laura Mathieu, résidant avenue Etienne Billieres 31300 Toulouse, de nationalité Française (France), née le 02/05/1995.
+            </Typography>
+            <Typography mt={4} textColor="white">
+                <strong>Hébergement</strong> :
+            </Typography>
+            <Typography textColor="white">
+                Le Site est hébergé par la société OVH SAS, situé 2 rue Kellermann - BP 80157 - 59053 Roubaix Cedex 1, (contact téléphonique ou email : 1007).
+            </Typography>
+            <Typography mt={4} textColor="white">
+                <strong>Responsable de publication</strong> : Laura Mathieu
+            </Typography>
+            <Typography mt={4} textColor="white">
+                <strong>Nous contacter</strong> :
+            </Typography>
+            <Typography textColor="white">
+                Par téléphone : +33683794378<br />
+                Par email : laura.mathieu@protonmail.com
+            </Typography>
+            <Typography mt={4} textColor="white">
+                <strong>Données personnelles</strong> :
+            </Typography>
+            <Typography textColor="white">
+                Une donnée à character personnel désigne toute information concernant une personne physique identifiée ou identifiable (personne concernée). Une personne identifiable est une personne qui peut être identifiée, directement ou indirectement, notamment par référence à un nom, un numéro d'identification ou à un ou plusieurs éléments spécifiques, propres à son identité physique, physiologique, génétique, psychique, économique, culturelle ou sociale.
+            </Typography>
+            <Typography textColor="white">
+                Les informations personnelles peuvent être recueillies lors de la navigation sur le site, notamment via le formulaire de contact. Les informations recueillies ne sont en aucun cas transmises à des tiers et ne sont utilisées que dans le cadre de la relation commerciale entre l'utilisateur et Laura Mathieu.
+            </Typography>
+            <Typography mt={4} textColor="white">
+                Les données personnelles collectées sont les suivantes :
+            </Typography>
+            <Typography textColor="white">
+                Nom et prénom<br />
+                Adresse mail
+            </Typography>
+            <Typography mt={4} textColor="white">
+                <strong>Loi applicable</strong> : Le présent site est soumis à la loi française.
+            </Typography>
+        </Box>
+    )
+}
