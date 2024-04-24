129c702d2128a3741e8c5e32a9eda145a26d9de4
Laura <laura@upschool.fr>
2024-04-24 08:51:03 +0200

    fix logo and language icon

--- src/views/common/language-selector/LanguageSelector.tsx
+++ src/views/common/language-selector/LanguageSelector.tsx
@@ -1,6 +1,7 @@
 import { Dropdown, Menu, MenuButton, MenuItem } from "@mui/joy";
 import { useTranslation } from "react-i18next";
 import LanguageIcon from '@mui/icons-material/Language';
+import { useMediaQuery } from "@mui/material";
 
 const countries = [
     {
@@ -16,12 +17,26 @@ const countries = [
 export default function LanguageSelector () {
     const { i18n } = useTranslation();
 
+    const isDesktop = useMediaQuery("(min-width:600px)");
+
+    const flag =
+    i18n.language === "en"
+      ? "/assets/flags/uk.png"
+      : "/assets/flags/france.png";
+
     return (
         <Dropdown>
-            <MenuButton variant="soft" sx={{margin: 0, padding: "0 10px", '&:hover': {
+            {/* <MenuButton variant="soft" sx={{margin: 0, padding: "0 10px", '&:hover': {
                         backgroundColor: "#61dafb", color: "white"
                     }}}>
                 <LanguageIcon />
+            </MenuButton> */}
+            <MenuButton variant="plain" sx={{color: "white"}}>
+                {isDesktop && (i18n.language === "en" ? "English" : "Français")}
+                {isDesktop ? 
+                    <img src={flag} width="25" style={{ marginLeft: "10px" }} /> :
+                    <img src={flag} width="25" />
+                }
             </MenuButton>
             <Menu>
                 {countries.map((lng) => {
